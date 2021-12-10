fc14bcf43dbb2a2ca294259ffb912d15fe1bed36
Mehmet Samil Erturk <mehmetsamilerturk@outlook.com>
2021-12-10 22:53:43 +0300

    version 1.1

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,10 +4,10 @@ p "Numbers: #{numbers}"
 def bubble_sort(numbers)
   n = numbers.length
 
-  while true do
+  loop do
     swapped = false
     i = 1
-    while i <= n - 1 do
+    while i <= n - 1
       if numbers[i - 1] > numbers[i]
         numbers[i - 1], numbers[i] = numbers[i], numbers[i - 1]
         swapped = true
@@ -15,7 +15,7 @@ def bubble_sort(numbers)
       i += 1
     end
     # Passes already sorted values to make the process more optimized
-    n = n - 1
+    n -= 1
     return numbers if swapped == false
   end
 end
